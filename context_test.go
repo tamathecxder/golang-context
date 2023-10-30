b4af25f56e59c5cfa56279f242c46cf286a77bcf
tamathecxder <yudistiraeka.pratama012@gmail.com>
2023-10-30 20:53:47 +0700

    store and retrieve data from context with context.WithValue

--- context_test.go
+++ context_test.go
@@ -6,6 +6,32 @@ import (
 	"testing"
 )
 
+func TestContextValue(t *testing.T) {
+	contextA := context.Background()
+
+	contextB := context.WithValue(contextA, "b", "B")
+	contextC := context.WithValue(contextA, "c", "C")
+
+	contextD := context.WithValue(contextB, "d", "D")
+	contextE := context.WithValue(contextB, "e", "E")
+
+	contextF := context.WithValue(contextC, "f", "F")
+
+	fmt.Println(contextA)
+	fmt.Println(contextB)
+	fmt.Println(contextC)
+	fmt.Println(contextD)
+	fmt.Println(contextE)
+	fmt.Println(contextF)
+
+	fmt.Println("-----------------------------------")
+
+	fmt.Println(contextF.Value("f")) // success
+	fmt.Println(contextF.Value("c")) // success, because C is F parent
+	fmt.Println(contextF.Value("b")) // fail, the parent is different
+	fmt.Println(contextA.Value("e")) // fail, parent cannot access value from its child
+}
+
 func TestContext(t *testing.T) {
 	background := context.Background()
 
