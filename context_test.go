c478dc61dde8d13f86716626400057dfcb005c03
tamathecxder <yudistiraeka.pratama012@gmail.com>
2023-10-31 22:32:09 +0700

    making a context.withCancel to inform other process to stop

--- context_test.go
+++ context_test.go
@@ -3,8 +3,56 @@ package golangcontext
 import (
 	"context"
 	"fmt"
+	"runtime"
 	"testing"
+	"time"
 )
+
+func CreateCounter(ctx context.Context) chan int {
+	destination := make(chan int)
+
+	go func() {
+		defer close(destination)
+		counter := 0
+
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+				destination <- counter
+				counter++
+			}
+		}
+	}()
+
+	return destination
+}
+
+func TestContextWithCancel(t *testing.T) {
+	parent := context.Background()
+	ctx, cancel := context.WithCancel(parent)
+
+	fmt.Println("before, total goroutine:", runtime.NumGoroutine())
+
+	destination := CreateCounter(ctx)
+
+	fmt.Println("process, total goroutine:", runtime.NumGoroutine())
+
+	for n := range destination {
+		fmt.Println("Counter:", n)
+
+		if n == 10 {
+			break
+		}
+	}
+
+	cancel()
+
+	time.Sleep(3 * time.Second)
+
+	fmt.Println("after, total goroutine:", runtime.NumGoroutine())
+}
 
 func TestContextValue(t *testing.T) {
 	contextA := context.Background()
